de1bf7a5c3025b4d8693bed6906d5c221c480429
GrowingChoi <csj563@naver.com>
2025-07-17 11:23:16 +0900

    [수정]404알림나던거 안나게 경로 수정함.

--- chat_project/chatbot/urls.py
+++ chat_project/chatbot/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.chat_interface, name='chat_interface'),
-    path('api/chat_message/<str:message>', views.chat_api, name='chat_api'),
+    path('chatbot/api/chat_message/<str:message>', views.chat_api, name='chat_api'),
 ]
