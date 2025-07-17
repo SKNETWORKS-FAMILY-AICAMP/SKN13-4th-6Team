2ce0ff5709e5d8f977a32f7cc0ace768715249a0
tnalsdk111 <a0121tnals@naver.com>
2025-07-17 14:00:47 +0900

    [수정]수민 브랜치 업데이트

--- chat_project/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chat_interface, name='chat_interface'),
-    path('api/chat_message/<str:message>', views.chat_api, name='chat_api'),
-]
